320915827c689b451571b85f74ff5f0518c2b514
jamie-sgro <jamie.sgro@uwaterloo.ca>
2020-05-01 16:18:28 -0400

    add DataLibrary

---
+++ DataLibrary/BusinessLogic/AccountProcessor.cs
@@ -0,0 +1,32 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic {
+  public static class AccountProcessor {
+    public static int CreateEmployee(int accountId, string firstName,
+      string lastName, string emailAddress) {
+      EmployeeModel data = new EmployeeModel {
+        EmployeeId = employeeId,
+        FirstName = firstName,
+        LastName = lastName,
+        EmailAddress = emailAddress
+      };
+
+      string sql = @"INSERT INTO dbo.AccountTable (AccountId, FirstName, LastName, EmailAddress
+                     VALUES (@AccountId, @FirstName, @LastName, @EmailAddress);";
+
+      return SqlDataAccess.SaveData(sql, data);
+    }
+
+    public static List<EmployeeModel> LoadEmployees() {
+      string sql = @"SELECT Id, AccountId, FirstName, LastName, EmailAddress
+                     FROM dbo.AccountTable;";
+      return SqlDataAccess.LoadData<EmployeeModel>(sql);
+    }
+  }
+}
---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace DataLibrary.DataAccess {
+  public static class SqlDataAccess {
+    public static string GetConnectionString(string connectionName = "mvcdb") {
+      return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+    }
+
+    public static List<T> LoadData<T>(string sql) {
+      using (MySqlConnection cnn = new MySqlConnection(GetConnectionString())) {
+        return cnn.Query<T>(sql).ToList();
+      }
+    }
+
+    public static int SaveData<T>(string sql, T data) {
+      using (MySqlConnection cnn = new MySqlConnection(GetConnectionString())) {
+        return cnn.Execute(sql, data);
+      }
+    }
+  }
+}
