7b765346e1fa98eb53315160e0d15d023fc0ef91
jamie-sgro <jamie.sgro@uwaterloo.ca>
2020-05-01 16:18:42 -0400

    added MVCApp files

---
+++ MVCApp/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using MVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary;
+using static DataLibrary.BusinessLogic.EmployeeProcessor;
+
+namespace MVCApp.Controllers {
+  public class HomeController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+
+    public ActionResult About() {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact() {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+
+    public ActionResult ViewEmployees() {
+      ViewBag.Message = "Employees List";
+
+      var data = LoadEmployees();
+      List<EmployeeModel> employees = new List<EmployeeModel>();
+
+      foreach (var row in data) {
+        employees.Add(new EmployeeModel {
+          EmployeeId = row.EmployeeId,
+          FirstName = row.FirstName,
+          LastName = row.LastName,
+          EmailAddress = row.EmailAddress,
+          ConfirmEmail = row.EmailAddress
+        });
+      }
+
+      return View(employees);
+    }
+
+    // GET
+    public ActionResult SignUp() {
+      ViewBag.Message = "Employee Sign Up";
+
+      return View();
+    }
+
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult SignUp(EmployeeModel model) {
+      if (ModelState.IsValid) {
+        int recordsCreated = CreateEmployee(
+          model.EmployeeId, 
+          model.FirstName, 
+          model.LastName, 
+          model.EmailAddress);
+        return RedirectToAction("Index");
+      }
+
+      return View();
+    }
+
+  }
+}
---
+++ MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models {
+  public class EmployeeModel {
+    [Display(Name = "Employee ID")]
+    [Range(100000,999999, ErrorMessage = "You need to enter a valid EmployeeId.")]
+    public int EmployeeId { get; set; }
+    
+    [Display(Name = "First Name")]
+    [Required(ErrorMessage = "You need to give us your first name.")]
+    public string FirstName { get; set; }
+    
+    [Display(Name = "Last Name")]
+    [Required(ErrorMessage = "You need to give us your last name.")]
+    public string LastName { get; set; }
+    
+    [DataType(DataType.EmailAddress)]
+    [Display(Name = "Email Addres")]
+    [Required(ErrorMessage = "You need to give us your email name.")]
+    public string EmailAddress { get; set; }
+    
+    [Compare("EmailAddress", ErrorMessage = "The Email and Confirm Email must match.")]
+    public string ConfirmEmail { get; set; }
+
+    [Display(Name = "Password")]
+    [Required(ErrorMessage = "You must have a password.")]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 10, ErrorMessage = "You need to provide an email between 10 and 100 characters in length.")]
+    public string Password { get; set; }
+    
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Your Password and Confirm Password do not match.")]
+    public string ConfirmPassword { get; set; }
+  }
+}
